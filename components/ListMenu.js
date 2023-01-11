d99b84659cec330e59ac2c29b91d61f6473c5bd3
Farihul Rouf <farihulrouf@gmail.com>
2023-01-11 11:54:00 +0700

    pixacloud 1.1

--- components/ListMenu.js
+++ components/ListMenu.js
@@ -7,9 +7,9 @@ const ListMenu = () => {
             <ul className="sm:relative sm:mr-8 mr-1 sm:flex sm:justify-end sm:py-0 sm:top-0 sm:space-x-5 absolute py-2 top-[60px] left-0 right-0">
                 {listMenu.map((item,index) =>
                     <li key={index} className="text-sm text-white sm:pl-0 sm:pb-0 pl-8 pb-1 font-palo hover:text-teal-300">
-                        {/*
-                        <Link href={item.link}><a>{item.text}</a></Link>
-                        */}
+                        {
+                        <a><Link href={item.link}>{item.text}</Link></a>
+                        }
                         </li>
                 )}
             </ul>
