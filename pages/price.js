0d7d014d1c9c6600468cd9284e82da421d47920d
Farihul Rouf <farihulrouf@gmail.com>
2023-01-15 12:53:31 +0700

    price

--- pages/price.js
+++ pages/price.js
@@ -12,6 +12,13 @@ const Price = () => {
             <div className="bg-[url('/img/bg_pages.svg')]">
             <div className="max-w-screen-xl mx-auto">
                     <div className="w-full p-4">
+
+                        <div className="mt-28 mb-8 text-white text-center">
+                            <h2 className='text-4xl font-semibold'>Pricing to fit your needs</h2>
+                            <p className='text-base'>Open 24/7, Pixarcloud offers flexe necessary amenities for a
+
+</p>
+                        </div>
                         <div className="grid grid-cols-12 gap-x-4 sm:gap-y-16 gap-4 mt-28 mb-8">
                            {price.map((item, index) => ( 
                                 <div key={index} className="lg:col-span-4 sm:col-span-6 col-span-12">
